0b0c50120db09f4fd6656f9ee904b152face77d5
Alice Souza <alicefms@hotmail.com>
2023-04-24 12:54:17 -0300

    Faz verificações no retorno do get.

--- Facts_test.js
+++ Facts_test.js
@@ -1,4 +1,47 @@
+const joi = require('joi');
+const chai = require('chai');
+
+
+const schemaFact = joi.object({
+    status: joi.object({
+        verified: joi.exist(),
+        sentCount: joi.number()
+    }),
+    _id: joi.string(),
+    user: joi.string(),
+    text: joi.string(),
+    type: joi.string(),
+    deleted: joi.boolean(),
+    createdAt: joi.string(),
+    updatedAt: joi.string(),
+    __v: joi.number(),
+    source: joi.string(),
+    used: joi.boolean()
+})
+
+
 Feature('Facts');
 
-Scenario('test something',  ({ I }) => {
+Scenario('Buscar um fato randômico',  async ({ I }) => {
+    response = await I.sendGetRequest('')
+    // Verificar se o código de status HTTP retornado é 200 OK.
+    I.seeResponseCodeIs(200)
+    // Verificar se a resposta da API é um JSON válido.
+    I.seeResponseMatchesJsonSchema(schemaFact)
+    // Verificar se o campo "text" contém um fato sobre gatos.
+    text = response.data.text
+    chai.assert.typeOf(text, 'string')
+    // Verificar se o campo "createdAt" contém a data e hora corretas.
+    dateCreatedAt = new Date(response.data.createdAt)
+    chai.assert.instanceOf(dateCreatedAt, Date, 'createdAt não é uma instancia de Data')
+    // Verificar se o campo "updatedAt" está vazio ou contém a data e hora corretas.
+    I.seeResponseValidByCallback(({data})=>{
+    chai.assert.isTrue(data.updatedAt == null || new Date (data.updatedAt) instanceof Date)})
 });
+
+
+
+
+
+
+
